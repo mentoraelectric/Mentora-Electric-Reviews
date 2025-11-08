d341437ec0743d6bf1f57f4c25fd9facfa1591e0
Mentora <mentoraelectric@gmail.com>
2025-11-08 14:01:40 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,8 +1,7 @@
-// Replace with your actual Supabase URL and anon key
-const supabaseUrl = 'https://zczyjceosielfmdkpldu.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjenlqY2Vvc2llbGZtZGtwbGR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIwMTM0MTAsImV4cCI6MjA3NzU4OTQxMH0.Zy3_O4Eoqhbdgg0pKDwugDMcK_xt36uDO1Ek_WueWPQ';
+// supabase-config.js
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
-// Create and export the Supabase client
-const supabase = window.supabase.createClient(supabaseUrl, supabaseAnonKey);
+const supabaseUrl = 'https://zczyjceosielfmdkpldu.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjenlqY2Vvc2llbGZtZGtwbGR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIwMTM0MTAsImV4cCI6MjA3NzU4OTQxMH0.Zy3_O4Eoqhbdgg0pKDwugDMcK_xt36uDO1Ek_WueWPQ'
 
-export { supabase };
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
